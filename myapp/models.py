203a118f56e08180f6bd922fef512a2f652ef7cd
Cemb93 <karloz13m@gmail.com>
2025-04-18 13:29:31 -0500

    Notas

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+# ! RELACION MUCHOS A MUCHOS = Teacher VS Subject
+# class Teacher(models.Model):
+#   teacherID = models.IntegerField(primary_key=True)
+#   qualification = models.CharField(max_length=50)
+#   email = models.EmailField(max_length=50)
+
+# class Subject(models.Model):
+#   subjectcode = models.IntegerField(primary_key = True)
+#   name = models.CharField(max_length=30)
+#   credits = models.IntegerField()
+#   teacher = models.ManyToManyField(Teacher)
+
+# ! RELACION UNO A MUCHOS = Teacher VS Subject
+# class Teacher(models.Model):
+#   TeacherID = models.IntegerField(primary_key=True)
+#   subjectcode=models.ForeignKey(Subject, on_delete=models.CASCADE)
+#   Qualification = models.CharField(max_length=50)
+#   email = models.EmailField(max_length=50)
+
+# class Subject(models.Model):
+#   Subjectcode = models.IntegerField(primary_key = True)
+#   name = models.CharField(max_length=30)
+#   credits = models.IntegerField()
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+  * CREAR
+  new_user = User(id=1, "John", "Jones")
+  new_user.save()
+  
+  ? ACTUALIZAR
+  user = User.objects.get(id=1)
+  user.last_name = "Smith"
+  user.save()
+  
+  ! ELIMINAR
+  user = User.objects.get(id=1)
+  user.delete()
+"""
