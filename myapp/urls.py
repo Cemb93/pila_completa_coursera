af3e65de82c728bedb82690ecd5281a2da43df9e
Cemb93 <karloz13m@gmail.com>
2025-04-19 22:01:57 -0500

    Iniciando proyecto final de pila completa

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
   # path('ratings', views.ratings),
+  path('', views.home, name="home"),
+  path('about/', views.about, name="about"),
+  path('book/', views.book, name="book"),
+  path('reservations/', views.reservations, name="reservations"),
+  path('menu/', views.menu, name="menu"),
+  path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
+  path('bookings', views.bookings, name='bookings'), 
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['restaurant/templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,8 +103,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = 'myapp/static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    "myapp/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
